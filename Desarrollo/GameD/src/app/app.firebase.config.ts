e5a9d254e7bc48af03f4c693ae6d6c03aad601ca
LauraM <lauramarcela089@gmail.com>
2018-08-03 19:48:55 -0500

    new change
    Prototipo

---
+++ Desarrollo/GameD/src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG ={
+    apiKey: "AIzaSyAUBlBp2s3Tt3nLFnGoL45214NEQrIsK1o",
+    authDomain: "gamed-d23e2.firebaseapp.com",
+    databaseURL: "https://gamed-d23e2.firebaseio.com",
+    projectId: "gamed-d23e2",
+    storageBucket: "gamed-d23e2.appspot.com",
+    messagingSenderId: "142788893536"
+};
